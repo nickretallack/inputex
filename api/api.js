68317db861c2ff1393d9770adbb99be256559935
neyric <eric.abouaf@gmail.com>
2012-08-17 18:30:34 +0200

    API doc

---
+++ api/api.js
@@ -0,0 +1,420 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "inputEx",
+        "inputEx.AutoComplete",
+        "inputEx.Base",
+        "inputEx.CheckBox",
+        "inputEx.ColorField",
+        "inputEx.CombineField",
+        "inputEx.DDListField",
+        "inputEx.DSSelectField",
+        "inputEx.DateField",
+        "inputEx.DatePickerField",
+        "inputEx.DateSelectMonthField",
+        "inputEx.DateSplitField",
+        "inputEx.DateTimeField",
+        "inputEx.EmailField",
+        "inputEx.Field",
+        "inputEx.FileField",
+        "inputEx.Form",
+        "inputEx.Group",
+        "inputEx.HiddenField",
+        "inputEx.IPv4Field",
+        "inputEx.ImageCropperField",
+        "inputEx.InPlaceEdit",
+        "inputEx.IntegerField",
+        "inputEx.JsonSchema",
+        "inputEx.JsonSchema.Builder",
+        "inputEx.KeyOpValueField",
+        "inputEx.KeyValueField",
+        "inputEx.Lens",
+        "inputEx.LinkedComboField",
+        "inputEx.ListField",
+        "inputEx.MapField",
+        "inputEx.MenuField",
+        "inputEx.MultiAutoComplete",
+        "inputEx.MultiSelectField",
+        "inputEx.NumberField",
+        "inputEx.ObjectField",
+        "inputEx.Panel",
+        "inputEx.PasswordField",
+        "inputEx.Plugin.InputExDataTable",
+        "inputEx.RPC",
+        "inputEx.RPC.Envelope",
+        "inputEx.RPC.Envelope.JSON",
+        "inputEx.RPC.Envelope.JSON-RPC-1.0",
+        "inputEx.RPC.Envelope.JSON-RPC-2.0",
+        "inputEx.RPC.Envelope.PATH",
+        "inputEx.RPC.Envelope.URL",
+        "inputEx.RPC.SMDTester",
+        "inputEx.RPC.Service",
+        "inputEx.RPC.Transport",
+        "inputEx.RTEField",
+        "inputEx.RadioField",
+        "inputEx.RatingStars",
+        "inputEx.RatingStarsForm",
+        "inputEx.SelectField",
+        "inputEx.SerializeField",
+        "inputEx.SerializeField.serializers",
+        "inputEx.SerializeField.serializers.json",
+        "inputEx.SerializeField.serializers.xml",
+        "inputEx.SliderField",
+        "inputEx.StringAvailability",
+        "inputEx.StringField",
+        "inputEx.TextAutoTag",
+        "inputEx.Textarea",
+        "inputEx.TimeField",
+        "inputEx.TimeIntervalField",
+        "inputEx.TinyMCEField",
+        "inputEx.TreeField",
+        "inputEx.TypeField",
+        "inputEx.UneditableField",
+        "inputEx.UpperCaseField",
+        "inputEx.UrlField",
+        "inputEx.VectorField",
+        "inputEx.mixin",
+        "inputEx.mixin.choice",
+        "inputEx.visus",
+        "inputEx.widget",
+        "inputEx.widget.Button",
+        "inputEx.widget.JsonTreeInspector"
+    ],
+    "modules": [
+        "inputex",
+        "inputex-autocomplete",
+        "inputex-base",
+        "inputex-button",
+        "inputex-checkbox",
+        "inputex-choice",
+        "inputex-color",
+        "inputex-combine",
+        "inputex-datatable",
+        "inputex-date",
+        "inputex-datepicker",
+        "inputex-dateselectmonth",
+        "inputex-datesplit",
+        "inputex-datetime",
+        "inputex-ddlist",
+        "inputex-dsselect",
+        "inputex-email",
+        "inputex-field",
+        "inputex-file",
+        "inputex-form",
+        "inputex-group",
+        "inputex-hidden",
+        "inputex-imagecropper",
+        "inputex-inplaceedit",
+        "inputex-integer",
+        "inputex-ipv4",
+        "inputex-jsonschema",
+        "inputex-jsontreeinspector",
+        "inputex-keyopvalue",
+        "inputex-keyvalue",
+        "inputex-lens",
+        "inputex-linkedcombo",
+        "inputex-list",
+        "inputex-map",
+        "inputex-menu",
+        "inputex-multiautocomplete",
+        "inputex-multiselect",
+        "inputex-number",
+        "inputex-object",
+        "inputex-panel",
+        "inputex-password",
+        "inputex-radio",
+        "inputex-ratingstars",
+        "inputex-ratingstarsform",
+        "inputex-rpc",
+        "inputex-rte",
+        "inputex-select",
+        "inputex-serialize",
+        "inputex-slider",
+        "inputex-smdtester",
+        "inputex-string",
+        "inputex-stringavailability",
+        "inputex-textarea",
+        "inputex-textautotag",
+        "inputex-time",
+        "inputex-timeinterval",
+        "inputex-timerange",
+        "inputex-tinymce",
+        "inputex-tree",
+        "inputex-type",
+        "inputex-uneditable",
+        "inputex-uppercase",
+        "inputex-url",
+        "inputex-vector",
+        "inputex-visus"
+    ],
+    "allModules": [
+        {
+            "displayName": "inputex",
+            "name": "inputex",
+            "description": "The inputEx Library"
+        },
+        {
+            "displayName": "inputex-autocomplete",
+            "name": "inputex-autocomplete"
+        },
+        {
+            "displayName": "inputex-base",
+            "name": "inputex-base",
+            "description": "A widget-stdmod-level extension that provides ability to render a form within the body"
+        },
+        {
+            "displayName": "inputex-button",
+            "name": "inputex-button"
+        },
+        {
+            "displayName": "inputex-checkbox",
+            "name": "inputex-checkbox"
+        },
+        {
+            "displayName": "inputex-choice",
+            "name": "inputex-choice"
+        },
+        {
+            "displayName": "inputex-color",
+            "name": "inputex-color"
+        },
+        {
+            "displayName": "inputex-combine",
+            "name": "inputex-combine"
+        },
+        {
+            "displayName": "inputex-datatable",
+            "name": "inputex-datatable",
+            "description": "The inputex-datatable module provides the inputEx.Plugin.InputExDataTable class which is a plugin."
+        },
+        {
+            "displayName": "inputex-date",
+            "name": "inputex-date"
+        },
+        {
+            "displayName": "inputex-datepicker",
+            "name": "inputex-datepicker"
+        },
+        {
+            "displayName": "inputex-dateselectmonth",
+            "name": "inputex-dateselectmonth"
+        },
+        {
+            "displayName": "inputex-datesplit",
+            "name": "inputex-datesplit"
+        },
+        {
+            "displayName": "inputex-datetime",
+            "name": "inputex-datetime"
+        },
+        {
+            "displayName": "inputex-ddlist",
+            "name": "inputex-ddlist"
+        },
+        {
+            "displayName": "inputex-dsselect",
+            "name": "inputex-dsselect"
+        },
+        {
+            "displayName": "inputex-email",
+            "name": "inputex-email"
+        },
+        {
+            "displayName": "inputex-field",
+            "name": "inputex-field",
+            "description": "Provides the base \"field\" abstract class"
+        },
+        {
+            "displayName": "inputex-file",
+            "name": "inputex-file"
+        },
+        {
+            "displayName": "inputex-form",
+            "name": "inputex-form"
+        },
+        {
+            "displayName": "inputex-group",
+            "name": "inputex-group"
+        },
+        {
+            "displayName": "inputex-hidden",
+            "name": "inputex-hidden"
+        },
+        {
+            "displayName": "inputex-imagecropper",
+            "name": "inputex-imagecropper"
+        },
+        {
+            "displayName": "inputex-inplaceedit",
+            "name": "inputex-inplaceedit"
+        },
+        {
+            "displayName": "inputex-integer",
+            "name": "inputex-integer"
+        },
+        {
+            "displayName": "inputex-ipv4",
+            "name": "inputex-ipv4"
+        },
+        {
+            "displayName": "inputex-jsonschema",
+            "name": "inputex-jsonschema"
+        },
+        {
+            "displayName": "inputex-jsontreeinspector",
+            "name": "inputex-jsontreeinspector"
+        },
+        {
+            "displayName": "inputex-keyopvalue",
+            "name": "inputex-keyopvalue"
+        },
+        {
+            "displayName": "inputex-keyvalue",
+            "name": "inputex-keyvalue"
+        },
+        {
+            "displayName": "inputex-lens",
+            "name": "inputex-lens"
+        },
+        {
+            "displayName": "inputex-linkedcombo",
+            "name": "inputex-linkedcombo"
+        },
+        {
+            "displayName": "inputex-list",
+            "name": "inputex-list"
+        },
+        {
+            "displayName": "inputex-map",
+            "name": "inputex-map"
+        },
+        {
+            "displayName": "inputex-menu",
+            "name": "inputex-menu"
+        },
+        {
+            "displayName": "inputex-multiautocomplete",
+            "name": "inputex-multiautocomplete"
+        },
+        {
+            "displayName": "inputex-multiselect",
+            "name": "inputex-multiselect"
+        },
+        {
+            "displayName": "inputex-number",
+            "name": "inputex-number"
+        },
+        {
+            "displayName": "inputex-object",
+            "name": "inputex-object"
+        },
+        {
+            "displayName": "inputex-panel",
+            "name": "inputex-panel",
+            "description": "Provides a Panel widget, a widget that mimics the functionality of a regular OS window.\nComes with Standard Module support, XY Positioning, Alignment Support, Stack (z-index) support,\nmodality, auto-focus and auto-hide functionality, and header/footer button support."
+        },
+        {
+            "displayName": "inputex-password",
+            "name": "inputex-password"
+        },
+        {
+            "displayName": "inputex-radio",
+            "name": "inputex-radio"
+        },
+        {
+            "displayName": "inputex-ratingstars",
+            "name": "inputex-ratingstars"
+        },
+        {
+            "displayName": "inputex-ratingstarsform",
+            "name": "inputex-ratingstarsform"
+        },
+        {
+            "displayName": "inputex-rpc",
+            "name": "inputex-rpc"
+        },
+        {
+            "displayName": "inputex-rte",
+            "name": "inputex-rte",
+            "description": "WARNING: this field requires YUI2 (for the YUI2 Riche Text Editor)"
+        },
+        {
+            "displayName": "inputex-select",
+            "name": "inputex-select"
+        },
+        {
+            "displayName": "inputex-serialize",
+            "name": "inputex-serialize"
+        },
+        {
+            "displayName": "inputex-slider",
+            "name": "inputex-slider"
+        },
+        {
+            "displayName": "inputex-smdtester",
+            "name": "inputex-smdtester"
+        },
+        {
+            "displayName": "inputex-string",
+            "name": "inputex-string"
+        },
+        {
+            "displayName": "inputex-stringavailability",
+            "name": "inputex-stringavailability"
+        },
+        {
+            "displayName": "inputex-textarea",
+            "name": "inputex-textarea"
+        },
+        {
+            "displayName": "inputex-textautotag",
+            "name": "inputex-textautotag"
+        },
+        {
+            "displayName": "inputex-time",
+            "name": "inputex-time"
+        },
+        {
+            "displayName": "inputex-timeinterval",
+            "name": "inputex-timeinterval"
+        },
+        {
+            "displayName": "inputex-timerange",
+            "name": "inputex-timerange"
+        },
+        {
+            "displayName": "inputex-tinymce",
+            "name": "inputex-tinymce"
+        },
+        {
+            "displayName": "inputex-tree",
+            "name": "inputex-tree"
+        },
+        {
+            "displayName": "inputex-type",
+            "name": "inputex-type"
+        },
+        {
+            "displayName": "inputex-uneditable",
+            "name": "inputex-uneditable"
+        },
+        {
+            "displayName": "inputex-uppercase",
+            "name": "inputex-uppercase"
+        },
+        {
+            "displayName": "inputex-url",
+            "name": "inputex-url"
+        },
+        {
+            "displayName": "inputex-vector",
+            "name": "inputex-vector"
+        },
+        {
+            "displayName": "inputex-visus",
+            "name": "inputex-visus",
+            "description": "Used by InPlaceEdit and Uneditable fields"
+        }
+    ]
+} };
+});
