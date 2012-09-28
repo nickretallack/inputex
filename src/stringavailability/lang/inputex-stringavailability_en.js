54c8060bf50c60c335945671f6f5f21c35b35f92
Benjamin NGUYEN <benjamin.kim.nguyen@gmail.com>
2012-09-28 17:11:49 +0200

    I18N - inputex-stringavailability - OK

---
+++ src/stringavailability/lang/inputex-stringavailability_en.js
@@ -0,0 +1,6 @@
+{
+   stringLoading: "Checking if available ...",
+   stringAvailable: "This ressource is available",
+   stringUnAvailable: "This ressource is not available",
+   errorDataText : "an error occurred"
+}
---
+++ src/stringavailability/lang/inputex-stringavailability_fr.js
@@ -0,0 +1,6 @@
+{
+   stringLoading: "Vérification de disponibilité ...",
+   stringAvailable: "La ressource est disponible !",
+   stringUnAvailable: "La ressource est indisponible",
+   errorDataText : "Une erreur s'est produite"
+}
