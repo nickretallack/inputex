560220fcd4321c2bfa44a9cdde83b06693fb4836
Benjamin NGUYEN <benjamin.kim.nguyen@gmail.com>
2012-10-01 10:49:47 +0200

    delete build & update .gitignore

--- build/inputex-panel/inputex-panel-min.js
+++
@@ -1,1 +0,0 @@
-YUI.add("inputex-panel",function(e,t){e.inputEx.Panel=e.Base.create("panel",e.Panel,[e.inputEx.Base],{hide:function(){e.inputEx.Panel.superclass.hide.apply(this,arguments),this.get("field").hide()},show:function(){e.inputEx.Panel.superclass.show.apply(this,arguments),this.get("field").show()}})},"@VERSION@",{requires:["inputex","panel","inputex-base"]});
