nickretallack/inputex

commit dbc385473a111712450868c197ee3f514f200c4d

Fix inputex-combine rendering & add validation to inputex-timerange

src/timerange/js/timerange.js
@@ -11,17 +11,16 @@
  * @module inputex-timerange
  */
 inputEx.TimeRange = function(options) {
-   
    var h1 = [], h2 = [], i, m = [], s;
-   for(i = 0 ; i < 25 ; i++) { 
-		s="";
-		if ( i<10 ){ s="0"; } 
-		s+= i;
-		h2.push({ value: s });
+
+   for(i = 0 ; i < 25 ; i++) {
+      s = i < 10 ?  "0" : "";
+      s += i;
+      h2.push({ value: s });
       if ( i<24 ){ h1.push({ value: s }); } // First block of hours musn't contain 24
-	}
-	m = [{ value: "00" },{ value: "05" },{ value: "10" },{ value: "15" },{ value: "20" },{ value: "25" },{ value: "30" },{ value: "35" },{ value: "40" },{ value: "45" },{ value: "50" },{ value: "55" }];
-	
+   }
+   m = [{ value: "00" },{ value: "05" },{ value: "10" },{ value: "15" },{ value: "20" },{ value: "25" },{ value: "30" },{ value: "35" },{ value: "40" },{ value: "45" },{ value: "50" },{ value: "55" }];
+   
    options.fields = [
       {type: 'select', choices: h1 },
       {type: 'select', choices: m },
@@ -32,16 +31,26 @@ inputEx.TimeRange = function(options) {
    options.separators = options.separators || [false,"H","&nbsp; à &nbsp;","H",false];
    inputEx.TimeRange.superclass.constructor.call(this,options);
 
-	// Hook toogleEndMinutes to the "updated" event of the 3d select
-	// Like that when the user selects/unselects 24h the minutes will toogle accordingly
-	var that = this;
-	this.inputs[2].on('updated',function(){
-		that.toogleEndMinutes();
-	});
-	
+
+   // Hook toggleEndMinutes to the "updated" event of the 3d select
+   // Like that when the user selects/unselects 24h the minutes will toogle accordingly
+   this.inputs[3].on('updated', this.toggleEndMinutes, this);
 };
 
-Y.extend(inputEx.TimeRange, inputEx.CombineField, {   
+Y.extend(inputEx.TimeRange, inputEx.CombineField, {
+
+   setOptions: function(options) {
+      inputEx.TimeRange.superclass.setOptions.call(this, options);
+
+      // I18N
+      this.messages = Y.mix(this.messages,Y.Intl.get("inputex-timerange"));
+   },
+
+   onChange: function () {
+      this.setClassFromState();
+      inputEx.TimeRange.superclass.onChange.call(this);
+   },
+
    /**
     * Returns an array like ["HH:MM","HH:MM"]
     * @method getValue
@@ -49,55 +58,73 @@ Y.extend(inputEx.TimeRange, inputEx.CombineField, {
    getValue: function() {
       var values = inputEx.TimeRange.superclass.getValue.call(this);
 
-		var returnedValue = [];
-		returnedValue.push(values[0]+":"+values[1]);
-		returnedValue.push(values[2]+":"+values[3]);
-		
+      var returnedValue = [];
+      returnedValue.push(values[0]+":"+values[1]);
+      returnedValue.push(values[2]+":"+values[3]);
+      
       return returnedValue;
    },
 
    /**
-    * Set the value 
+    * Set the value
     * @method setValue
     * @param {array} array with 4 Hour strings in display order (format ["HH","MM", "HH","MM"])
     * @param {boolean} [sendUpdatedEvt] (optional) Wether this setValue should fire the 'updated' event or not (default is true, pass false to NOT send the event)
     */
    setValue: function(arr, sendUpdatedEvt) {
-		var values = arr[0].split(":").concat(arr[1].split(":"));
+      var values = arr[0].split(":").concat(arr[1].split(":"));
       inputEx.TimeRange.superclass.setValue.call(this, values, sendUpdatedEvt);
-		this.toogleEndMinutes();
+      this.toggleEndMinutes();
    },
 
-	/**
-	 * Disable the last selector and set it to "00" when the third one's value is 24
-	 * (it will be inccorect to have an end superior to 24:00)
-	 * @method toggleEndMinutes
-	 */
-	toogleEndMinutes: function(){		
-		var endHours = this.inputs[2];
-		var endMinutes = this.inputs[3];
-		
-		if( endHours.getValue() == '24' ){ endMinutes.setValue("00"); endMinutes.disable();}
-		else{ endMinutes.enable(); }		
-	},
+   /**
+    * Disable the last selector and set it to "00" when the third one's value is 24
+    * (it will be inccorect to have an end superior to 24:00)
+    * @method toggleEndMinutes
+    */
+   toggleEndMinutes: function(){
+      var endHours   = this.inputs[2],
+          endMinutes = this.inputs[3];
+      
+      if (endHours.getValue() === '24') {
+         endMinutes.setValue("00", false);
+         endMinutes.disable();
+      }
+      else {
+         endMinutes.enable();
+      }
+   },
+
+   /**
+    * Add the minLength string message handling
+    * @method getStateString
+    */
+   getStateString: function(state) {
+      var values = this.getValue();
+
+      if (state === inputEx.stateInvalid && values[0] >= values[1]) {
+         return this.messages.incorrectOrder;
+      }
+      return inputEx.TimeRange.superclass.getStateString.call(this, state);
+   },
 
    /**
     * @method validate
     */
-	validate: function(){
-		var values = this.getValue();
-		
-		var hm = values[1].split(":");
-		if(hm[0] == '24' && hm[1] != '00'){
-			return false;
-		}
-		
-		if(values[0] >= values[1]){
-			return false;	
-		}
-		
-		return true;
-	}
+   validate: function(){
+      var values = this.getValue();
+      
+      var hm = values[1].split(":");
+      if(hm[0] == '24' && hm[1] != '00'){
+         return false;
+      }
+      
+      if(values[0] >= values[1]){
+         return false;
+      }
+      
+      return true;
+   }
 
 });
 

build/inputex-combine/inputex-combine-debug.js
@@ -36,48 +36,27 @@ Y.extend(inputEx.CombineField, inputEx.Group, {
       this.options.separators = options.separators;
    },
 
+   // Override Group.render()
+   render: function () {
+      return inputEx.Field.prototype.render.apply(this, arguments);
+   },
+
    /**
     * @method render
     */
-   render: function() {
-
-      // Create the div wrapper for this group
-      this.divEl = inputEx.cn('div', {
-         className: this.options.className
-      });
-      if(this.options.id) {
-         this.divEl.id = this.options.id;
-      }
-
-      // Label element
-      if(lang.isString(this.options.label)) {
-         this.labelDiv = inputEx.cn('div', {
-            id: this.divEl.id + '-label',
-            className: 'inputEx-label',
-            'for': this.divEl.id + '-field'
-         });
-         this.labelEl = inputEx.cn('label', null, null, this.options.label === "" ? "&nbsp;" : this.options.label);
-         this.labelDiv.appendChild(this.labelEl);
-         this.divEl.appendChild(this.labelDiv);
-      }
-
-      this.renderFields();
+   renderComponent: function() {
+      this.renderFields(this.fieldContainer);
 
       if(this.options.disabled) {
          this.disable();
       }
-
-      // Insert a float breaker
-      this.divEl.appendChild(inputEx.cn('div', {
-         className: "inputEx-clear-div"
-      }, null, " "));
    },
 
    /**
     * Render the subfields
     * @method renderFields
     */
-   renderFields: function() {
+   renderFields: function(parentNode) {
 
       this.appendSeparator(0);
 
@@ -111,7 +90,7 @@ Y.extend(inputEx.CombineField, inputEx.Group, {
          */
          Y.one(fieldEl).setStyle('float', 'left');
          
-         this.divEl.appendChild(fieldEl);
+         parentNode.appendChild(fieldEl);
 
          this.appendSeparator(i + 1);
       }
@@ -157,7 +136,7 @@ Y.extend(inputEx.CombineField, inputEx.Group, {
    },
 
    /**
-    * Add a separator to the divEl
+    * Add a separator to the fieldContainer
     * @method appendSeparator
     */
    appendSeparator: function(i) {
@@ -165,7 +144,7 @@ Y.extend(inputEx.CombineField, inputEx.Group, {
          var sep = inputEx.cn('div', {
             className: 'inputEx-CombineField-separator'
          }, null, this.options.separators[i]);
-         this.divEl.appendChild(sep);
+         this.fieldContainer.appendChild(sep);
       }
    },
 
@@ -179,7 +158,7 @@ Y.extend(inputEx.CombineField, inputEx.Group, {
 
       inputEx.CombineField.superclass.initEvents.apply(this, arguments);
 
-      divNode = Y.one(this.divEl);
+      divNode = Y.one(this.fieldContainer);
 
       // TODO: does it work ?
       divNode.on("focusout", function(e) {
@@ -222,6 +201,8 @@ Y.extend(inputEx.CombineField, inputEx.Group, {
 
       this.runFieldsInteractions();
 
+      this.setClassFromState();
+      
       if(sendUpdatedEvt !== false) {
          // fire update event
          this.fireUpdatedEvt();

build/inputex-combine/inputex-combine-min.js
@@ -1,1 +1,1 @@
-YUI.add("inputex-combine",function(e,t){var n=e.Lang,r=e.inputEx;r.CombineField=function(e){r.CombineField.superclass.constructor.call(this,e)},e.extend(r.CombineField,r.Group,{setOptions:function(e){r.CombineField.superclass.setOptions.call(this,e),this.options.className=e.className?e.className:"inputEx-CombineField",this.options.separators=e.separators},render:function(){this.divEl=r.cn("div",{className:this.options.className}),this.options.id&&(this.divEl.id=this.options.id),n.isString(this.options.label)&&(this.labelDiv=r.cn("div",{id:this.divEl.id+"-label",className:"inputEx-label","for":this.divEl.id+"-field"}),this.labelEl=r.cn("label",null,null,this.options.label===""?"&nbsp;":this.options.label),this.labelDiv.appendChild(this.labelEl),this.divEl.appendChild(this.labelDiv)),this.renderFields(),this.options.disabled&&this.disable(),this.divEl.appendChild(r.cn("div",{className:"inputEx-clear-div"},null," "))},renderFields:function(){this.appendSeparator(0);if(!this.options.fields)return;var t,n=this.options.fields.length,r,i,s,o;for(t=0;t<n;t++)r=this.options.fields[t],i=this.renderField(r),s=i.getEl(),o=r.type,o!=="group"&&o!=="form"&&s.removeChild(s.childNodes[s.childNodes.length-1]),e.one(s).setStyle("float","left"),this.divEl.appendChild(s),this.appendSeparator(t+1);this.setFieldName(this.options.name)},renderField:function(e){return this.options.required&&(e.required=!0),r.CombineField.superclass.renderField.call(this,e)},setFieldName:function(e){var t,n;if(e)for(t=0;t<this.inputs.length;t++)n="",this.inputs[t].options.name?n=e+"["+this.inputs[t].options.name+"]":n=e+"["+t+"]",this.inputs[t].setFieldName(n)},appendSeparator:function(e){if(this.options.separators&&this.options.separators[e]){var t=r.cn("div",{className:"inputEx-CombineField-separator"},null,this.options.separators[e]);this.divEl.appendChild(t)}},initEvents:function(){var t=this,i,s;r.CombineField.superclass.initEvents.apply(this,arguments),s=e.one(this.divEl),s.on("focusout",function(e){e=n.merge(e),i=window.setTimeout(function(){i=null,t.onBlur(e)},25)}),s.on("focusin",function(e){i!==null?(window.clearTimeout(i),i=null):t.onFocus(e)})},setValue:function(e,t){if(!e)return;var n,r=this.inputs.length;for(n=0;n<r;n++)this.inputs[n].setValue(e[n],!1);this.runFieldsInteractions(),t!==!1&&this.fireUpdatedEvt()},getValue:function(){var e=[],t,n=this.inputs.length;for(t=0;t<n;t++)e.push(this.inputs[t].getValue());return e}}),r.registerType("combine",r.CombineField,[{type:"list",name:"fields",label:"Elements",required:!0,elementType:{type:"type"}},{type:"list",name:"separators",label:"Separators",required:!0}])},"@VERSION@",{requires:["inputex-group"],ix_provides:"combine",skinnable:!0});
+YUI.add("inputex-combine",function(e,t){var n=e.Lang,r=e.inputEx;r.CombineField=function(e){r.CombineField.superclass.constructor.call(this,e)},e.extend(r.CombineField,r.Group,{setOptions:function(e){r.CombineField.superclass.setOptions.call(this,e),this.options.className=e.className?e.className:"inputEx-CombineField",this.options.separators=e.separators},render:function(){return r.Field.prototype.render.apply(this,arguments)},renderComponent:function(){this.renderFields(this.fieldContainer),this.options.disabled&&this.disable()},renderFields:function(t){this.appendSeparator(0);if(!this.options.fields)return;var n,r=this.options.fields.length,i,s,o,u;for(n=0;n<r;n++)i=this.options.fields[n],s=this.renderField(i),o=s.getEl(),u=i.type,u!=="group"&&u!=="form"&&o.removeChild(o.childNodes[o.childNodes.length-1]),e.one(o).setStyle("float","left"),t.appendChild(o),this.appendSeparator(n+1);this.setFieldName(this.options.name)},renderField:function(e){return this.options.required&&(e.required=!0),r.CombineField.superclass.renderField.call(this,e)},setFieldName:function(e){var t,n;if(e)for(t=0;t<this.inputs.length;t++)n="",this.inputs[t].options.name?n=e+"["+this.inputs[t].options.name+"]":n=e+"["+t+"]",this.inputs[t].setFieldName(n)},appendSeparator:function(e){if(this.options.separators&&this.options.separators[e]){var t=r.cn("div",{className:"inputEx-CombineField-separator"},null,this.options.separators[e]);this.fieldContainer.appendChild(t)}},initEvents:function(){var t=this,i,s;r.CombineField.superclass.initEvents.apply(this,arguments),s=e.one(this.fieldContainer),s.on("focusout",function(e){e=n.merge(e),i=window.setTimeout(function(){i=null,t.onBlur(e)},25)}),s.on("focusin",function(e){i!==null?(window.clearTimeout(i),i=null):t.onFocus(e)})},setValue:function(e,t){if(!e)return;var n,r=this.inputs.length;for(n=0;n<r;n++)this.inputs[n].setValue(e[n],!1);this.runFieldsInteractions(),this.setClassFromState(),t!==!1&&this.fireUpdatedEvt()},getValue:function(){var e=[],t,n=this.inputs.length;for(t=0;t<n;t++)e.push(this.inputs[t].getValue());return e}}),r.registerType("combine",r.CombineField,[{type:"list",name:"fields",label:"Elements",required:!0,elementType:{type:"type"}},{type:"list",name:"separators",label:"Separators",required:!0}])},"@VERSION@",{requires:["inputex-group"],ix_provides:"combine",skinnable:!0});

build/inputex-combine/inputex-combine.js
@@ -36,48 +36,27 @@ Y.extend(inputEx.CombineField, inputEx.Group, {
       this.options.separators = options.separators;
    },
 
+   // Override Group.render()
+   render: function () {
+      return inputEx.Field.prototype.render.apply(this, arguments);
+   },
+
    /**
     * @method render
     */
-   render: function() {
-
-      // Create the div wrapper for this group
-      this.divEl = inputEx.cn('div', {
-         className: this.options.className
-      });
-      if(this.options.id) {
-         this.divEl.id = this.options.id;
-      }
-
-      // Label element
-      if(lang.isString(this.options.label)) {
-         this.labelDiv = inputEx.cn('div', {
-            id: this.divEl.id + '-label',
-            className: 'inputEx-label',
-            'for': this.divEl.id + '-field'
-         });
-         this.labelEl = inputEx.cn('label', null, null, this.options.label === "" ? "&nbsp;" : this.options.label);
-         this.labelDiv.appendChild(this.labelEl);
-         this.divEl.appendChild(this.labelDiv);
-      }
-
-      this.renderFields();
+   renderComponent: function() {
+      this.renderFields(this.fieldContainer);
 
       if(this.options.disabled) {
          this.disable();
       }
-
-      // Insert a float breaker
-      this.divEl.appendChild(inputEx.cn('div', {
-         className: "inputEx-clear-div"
-      }, null, " "));
    },
 
    /**
     * Render the subfields
     * @method renderFields
     */
-   renderFields: function() {
+   renderFields: function(parentNode) {
 
       this.appendSeparator(0);
 
@@ -111,7 +90,7 @@ Y.extend(inputEx.CombineField, inputEx.Group, {
          */
          Y.one(fieldEl).setStyle('float', 'left');
          
-         this.divEl.appendChild(fieldEl);
+         parentNode.appendChild(fieldEl);
 
          this.appendSeparator(i + 1);
       }
@@ -157,7 +136,7 @@ Y.extend(inputEx.CombineField, inputEx.Group, {
    },
 
    /**
-    * Add a separator to the divEl
+    * Add a separator to the fieldContainer
     * @method appendSeparator
     */
    appendSeparator: function(i) {
@@ -165,7 +144,7 @@ Y.extend(inputEx.CombineField, inputEx.Group, {
          var sep = inputEx.cn('div', {
             className: 'inputEx-CombineField-separator'
          }, null, this.options.separators[i]);
-         this.divEl.appendChild(sep);
+         this.fieldContainer.appendChild(sep);
       }
    },
 
@@ -179,7 +158,7 @@ Y.extend(inputEx.CombineField, inputEx.Group, {
 
       inputEx.CombineField.superclass.initEvents.apply(this, arguments);
 
-      divNode = Y.one(this.divEl);
+      divNode = Y.one(this.fieldContainer);
 
       // TODO: does it work ?
       divNode.on("focusout", function(e) {
@@ -222,6 +201,8 @@ Y.extend(inputEx.CombineField, inputEx.Group, {
 
       this.runFieldsInteractions();
 
+      this.setClassFromState();
+      
       if(sendUpdatedEvt !== false) {
          // fire update event
          this.fireUpdatedEvt();

build/inputex-loader/inputex-loader-debug.js
@@ -704,7 +704,12 @@ YUI().use(function(Y) {
     },
     "inputex-timerange": {
         "ix_provides": "timerange",
+        "lang": [
+            "en",
+            "fr"
+        ],
         "requires": [
+            "intl",
             "inputex-combine",
             "inputex-select"
         ]

build/inputex-loader/inputex-loader-min.js
@@ -1,2 +1,2 @@
 YUI().use(function(e){var t={groups:{inputex:{base:"inputex/src/",combine:!1,modules:{inputex:{requires:["intl","node","plugin","pluginhost-base","pluginhost-config","base-pluginhost","node-pluginhost"],skinnable:!0},"inputex-autocomplete":{ix_provides:"autocomplete",requires:["inputex-string","autocomplete"],skinnable:!0},"inputex-base":{requires:["inputex","widget","widget-stdmod"]},"inputex-builder":{requires:["inputex","inputex-autocomplete","inputex-base","inputex-button","inputex-checkbox","inputex-choice","inputex-color","inputex-combine","inputex-datatable","inputex-date","inputex-datepicker","inputex-dateselectmonth","inputex-datesplit","inputex-datetime","inputex-ddlist","inputex-dsselect","inputex-email","inputex-field","inputex-file","inputex-form","inputex-group","inputex-hidden","inputex-imagecropper","inputex-inplaceedit","inputex-integer","inputex-ipv4","inputex-jsonschema","inputex-jsontreeinspector","inputex-keyopvalue","inputex-keyvalue","inputex-lens","inputex-linkedcombo","inputex-list","inputex-map","inputex-menu","inputex-multiautocomplete","inputex-multiselect","inputex-number","inputex-object","inputex-panel","inputex-password","inputex-radio","inputex-ratingstars","inputex-ratingstarsform","inputex-rpc","inputex-rte","inputex-select","inputex-serialize","inputex-slider","inputex-smdtester","inputex-string","inputex-stringavailability","inputex-textarea","inputex-textautotag","inputex-time","inputex-timeinterval","inputex-timerange","inputex-tinymce","inputex-tree","inputex-type","inputex-uneditable","inputex-uppercase","inputex-url","inputex-vector","inputex-visus","dd-plugin"],skinnable:!0},"inputex-button":{requires:["inputex"]},"inputex-calendar-today-highlight-plugin":{requires:["plugin","calendar","datatype-date-math"],skinnable:!0},"inputex-calendar-week-number-plugin":{lang:["en","fr"],requires:["plugin","calendar","datatype-date-math"],skinnable:!0},"inputex-calendar-year-navigator-plugin":{lang:["en","fr"],requires:["intl","node-base","node-event-delegate","plugin","panel","inputex-group","inputex-select","inputex-string"],skinnable:!0},"inputex-checkbox":{ix_provides:"boolean",requires:["inputex-field"],skinnable:!0},"inputex-choice":{requires:["inputex"]},"inputex-color":{ix_provides:"color",requires:["inputex-field","node-event-delegate","event-outside","overlay"],skinnable:!0},"inputex-combine":{ix_provides:"combine",requires:["inputex-group"],skinnable:!0},"inputex-datatable":{lang:["en","fr","de","ca","es","fr","it","nl"],requires:["intl","node-event-delegate","inputex-group","inputex-panel","datatable","overlay"],skinnable:!0},"inputex-date":{ix_provides:"date",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-string"],skinnable:!0},"inputex-datepicker":{ix_provides:"datepicker",requires:["inputex-date","event-outside","node-event-delegate","overlay","calendar"],skinnable:!0},"inputex-dateselectmonth":{ix_provides:"dateselectmonth",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-combine","inputex-string","inputex-select"]},"inputex-datesplit":{ix_provides:"datesplit",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-combine","inputex-integer"]},"inputex-datetime":{ix_provides:"datetime",requires:["inputex-datepicker","inputex-combine","inputex-time"]},"inputex-ddlist":{requires:["inputex-field","array-extras","sortable","substitute"],skinnable:!0},"inputex-dsselect":{ix_provides:"dsselect",requires:["inputex-select","datasource"]},"inputex-email":{ix_provides:"email",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-string"]},"inputex-field":{lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex","intl"],skinnable:!0},"inputex-file":{ix_provides:"file",requires:["inputex-field"]},"inputex-form":{ix_provides:"form",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["io-base","json-stringify","inputex-group","inputex-button"],skinnable:!0},"inputex-group":{ix_provides:"group",requires:["inputex-field"],skinnable:!0},"inputex-hidden":{ix_provides:"hidden",requires:["inputex-field"]},"inputex-imagecropper":{ix_provides:"imagecropper",requires:["inputex-field"],skinnable:!0},"inputex-inplaceedit":{ix_provides:"inplaceedit",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-field","inputex-button","anim-base","anim-color","inputex-visus"],skinnable:!0},"inputex-integer":{ix_provides:"integer",requires:["inputex-string"]},"inputex-ipv4":{ix_provides:"ipv4",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-string"]},"inputex-jsonschema":{requires:["inputex"]},"inputex-jsontreeinspector":{requires:["inputex"],skinnable:!0},"inputex-keyopvalue":{ix_provides:"keyopvalue",requires:["inputex-keyvalue"]},"inputex-keyvalue":{ix_provides:"keyvalue",requires:["inputex-combine"]},"inputex-lens":{ix_provides:"lens",requires:["inputex-group","inputex-inplaceedit"]},"inputex-linkedcombo":{requires:["inputex-select","inputex-choice"]},"inputex-list":{ix_provides:"list",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-field","anim-base","anim-color"],skinnable:!0},"inputex-map":{ix_provides:"map",requires:["inputex-field"]},"inputex-menu":{ix_provides:"menu",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-field","node-event-delegate","node-menunav-improved","substitute"],skinnable:!0},"inputex-multiautocomplete":{ix_provides:"multiautocomplete",requires:["inputex-autocomplete","json","inputex-ddlist"]},"inputex-multiselect":{ix_provides:"multiselect",requires:["inputex-select","inputex-ddlist"]},"inputex-number":{ix_provides:"number",requires:["inputex-string"]},"inputex-object":{ix_provides:"object",requires:["inputex-list","inputex-combine","inputex-string"]},"inputex-panel":{requires:["inputex","panel","inputex-base"]},"inputex-password":{ix_provides:"password",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-string"],skinnable:!0},"inputex-radio":{ix_provides:"radio",requires:["selector","node-event-delegate"
-,"inputex-field","inputex-choice","inputex-string"],skinnable:!0},"inputex-ratingstars":{ix_provides:"ratingstars",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-field"],skinnable:!0},"inputex-ratingstarsform":{ix_provides:"ratingstarsform",requires:["inputex-ratingstars","inputex-form"]},"inputex-rpc":{requires:["json","inputex","io-base","inputex-jsonschema","jsonp"]},"inputex-rte":{ix_provides:"html",requires:["inputex-field","yui2-editor"]},"inputex-select":{ix_provides:"select",requires:["inputex-field","inputex-choice"]},"inputex-serialize":{ix_provides:"serialize",requires:["inputex-string","json"]},"inputex-slider":{ix_provides:"slider",requires:["inputex-field","slider"],skinnable:!0},"inputex-smdtester":{requires:["inputex-rpc","inputex-jsontreeinspector"]},"inputex-string":{ix_provides:"string",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-field","event-key"],skinnable:!0},"inputex-stringavailability":{lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-string","event-key","io-base","json-parse"],skinnable:!0},"inputex-textarea":{ix_provides:"text",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-string"]},"inputex-textautotag":{ix_provides:"autotag",requires:["inputex-textarea"]},"inputex-time":{ix_provides:"time",requires:["inputex-combine","inputex-select"]},"inputex-timeinterval":{ix_provides:"timeinterval",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-combine","inputex-select"]},"inputex-timerange":{ix_provides:"timerange",requires:["inputex-combine","inputex-select"]},"inputex-tinymce":{ix_provides:"tinymce",requires:["inputex-field"]},"inputex-tree":{ix_provides:"tree",requires:["inputex-string","inputex-list","inputex-inplaceedit"]},"inputex-type":{ix_provides:"type",requires:["inputex-field","inputex-group","inputex-select","inputex-list","inputex-string","inputex-checkbox","inputex-integer"],skinnable:!0},"inputex-uneditable":{ix_provides:"uneditable",requires:["inputex-field","inputex-visus"]},"inputex-uppercase":{ix_provides:"uppercase",requires:["inputex-string"]},"inputex-url":{ix_provides:"url",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-string"],skinnable:!0},"inputex-vector":{ix_provides:"vector",requires:["inputex-combine"]},"inputex-visus":{requires:["inputex","dump"]},"node-menunav-improved":{requires:["event-resize","node-menunav"]}}}}};typeof YUI_config=="undefined"&&(YUI_config={groups:{}}),typeof YUI_config.groups=="undefined"&&(YUI_config.groups={}),e.mix(YUI_config.groups,t.groups);var n=YUI_config.groups.inputex.modules,r=[],i={};for(var s in n)n.hasOwnProperty(s)&&(r.push(s),n[s].ix_provides&&(i[n[s].ix_provides]=s));YUI_config.groups.inputex.allModules=r,YUI_config.groups.inputex.modulesByType=i});
+,"inputex-field","inputex-choice","inputex-string"],skinnable:!0},"inputex-ratingstars":{ix_provides:"ratingstars",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-field"],skinnable:!0},"inputex-ratingstarsform":{ix_provides:"ratingstarsform",requires:["inputex-ratingstars","inputex-form"]},"inputex-rpc":{requires:["json","inputex","io-base","inputex-jsonschema","jsonp"]},"inputex-rte":{ix_provides:"html",requires:["inputex-field","yui2-editor"]},"inputex-select":{ix_provides:"select",requires:["inputex-field","inputex-choice"]},"inputex-serialize":{ix_provides:"serialize",requires:["inputex-string","json"]},"inputex-slider":{ix_provides:"slider",requires:["inputex-field","slider"],skinnable:!0},"inputex-smdtester":{requires:["inputex-rpc","inputex-jsontreeinspector"]},"inputex-string":{ix_provides:"string",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-field","event-key"],skinnable:!0},"inputex-stringavailability":{lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-string","event-key","io-base","json-parse"],skinnable:!0},"inputex-textarea":{ix_provides:"text",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-string"]},"inputex-textautotag":{ix_provides:"autotag",requires:["inputex-textarea"]},"inputex-time":{ix_provides:"time",requires:["inputex-combine","inputex-select"]},"inputex-timeinterval":{ix_provides:"timeinterval",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-combine","inputex-select"]},"inputex-timerange":{ix_provides:"timerange",lang:["en","fr"],requires:["intl","inputex-combine","inputex-select"]},"inputex-tinymce":{ix_provides:"tinymce",requires:["inputex-field"]},"inputex-tree":{ix_provides:"tree",requires:["inputex-string","inputex-list","inputex-inplaceedit"]},"inputex-type":{ix_provides:"type",requires:["inputex-field","inputex-group","inputex-select","inputex-list","inputex-string","inputex-checkbox","inputex-integer"],skinnable:!0},"inputex-uneditable":{ix_provides:"uneditable",requires:["inputex-field","inputex-visus"]},"inputex-uppercase":{ix_provides:"uppercase",requires:["inputex-string"]},"inputex-url":{ix_provides:"url",lang:["en","fr","de","ca","es","fr","it","nl"],requires:["inputex-string"],skinnable:!0},"inputex-vector":{ix_provides:"vector",requires:["inputex-combine"]},"inputex-visus":{requires:["inputex","dump"]},"node-menunav-improved":{requires:["event-resize","node-menunav"]}}}}};typeof YUI_config=="undefined"&&(YUI_config={groups:{}}),typeof YUI_config.groups=="undefined"&&(YUI_config.groups={}),e.mix(YUI_config.groups,t.groups);var n=YUI_config.groups.inputex.modules,r=[],i={};for(var s in n)n.hasOwnProperty(s)&&(r.push(s),n[s].ix_provides&&(i[n[s].ix_provides]=s));YUI_config.groups.inputex.allModules=r,YUI_config.groups.inputex.modulesByType=i});

commit 1f3935ee7505851dc024c416da446e61f611307f

[inputex-timerange] broken since dbc3854

src/timerange/js/timerange.js
@@ -34,7 +34,7 @@ inputEx.TimeRange = function(options) {
 
    // Hook toggleEndMinutes to the "updated" event of the 3d select
    // Like that when the user selects/unselects 24h the minutes will toogle accordingly
-   this.inputs[3].on('updated', this.toggleEndMinutes, this);
+   this.inputs[2].on('updated', this.toggleEndMinutes, this);
 };
 
 Y.extend(inputEx.TimeRange, inputEx.CombineField, {