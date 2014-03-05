20b8d89f62746d40d7f22bc0cef70f3b9f2fa19b
Charly POLY <cpoly55@gmail.com>
2014-03-05 10:46:35 +0100

    fix(multiselect): complete refactor of multiselect module
    inputEx.MultiSelectField inherited from inputEx.SelectField and that was a bad design
    cause value come from the DDList, not from the select
    
    So now, the inputEx.MultiSelectField inherit from inputEx.DDListField for a more
    logical API

--- src/multiselect/js/multiselect.js
+++ src/multiselect/js/multiselect.js
@@ -1,179 +1,168 @@
 /**
  * @module inputex-multiselect
  */
-   var inputEx = Y.inputEx,
-       lang = Y.Lang;
-   
+ var inputEx = Y.inputEx,
+ lang = Y.Lang;
+
+/**
+ * Create a multi select field
+ * @class inputEx.MultiSelectField
+ * @extends inputEx.DDListField
+ * @constructor
+ * @param {Object} options Added options:
+ * <ul>
+ *    <li>choices: contains the list of choices configs ([{value:'usa'}, {value:'fr', label:'France'}])</li>
+ * </ul>
+ */
+inputEx.MultiSelectField = function (options) {
+  this.choices = options.choices;
+    this.options = options; // for compatibility with inputEx.Field API
+    inputEx.MultiSelectField.superclass.constructor.call(this, options);
+};
+
+Y.extend(inputEx.MultiSelectField, inputEx.DDListField, {
+
     /**
-     * Create a multi select field
-     * @class inputEx.MultiSelectField
-     * @extends inputEx.SelectField
-     * @constructor
-     * @param {Object} options Added options:
-     * <ul>
-     *    <li>choices: contains the list of choices configs ([{value:'usa'}, {value:'fr', label:'France'}])</li>
-     * </ul>
+     * Build the DDList
+     * @method renderComponent
      */
-    inputEx.MultiSelectField = function (options) {
-        inputEx.MultiSelectField.superclass.constructor.call(this, options);
-    };
+    renderComponent: function () {
 
-    Y.extend(inputEx.MultiSelectField, inputEx.SelectField, {
+        this.select = new inputEx.SelectField({
+            parentEl  : this.fieldContainer,
+            choices   : this.choices
+        });
 
-        /**
-         * Build the DDList
-         * @method renderComponent
-         */
-        renderComponent: function () {
+        // render DDList
+        inputEx.MultiSelectField.superclass.renderComponent.call(this);
 
-            inputEx.MultiSelectField.superclass.renderComponent.call(this);
+    },
 
-            this.ddlist = new inputEx.DDListField({
-                parentEl: this.fieldContainer,
-                removable: true
+    /**
+     * Register the "change" event
+     * @method initEvents
+     */
+    initEvents: function () {
+        this.select.on("updated", this.onAddNewItem, this);
+        this.on("itemRemoved", this.onItemRemoved, this);
+    },
+
+    /**
+     * Re-enable the option element when an item is removed by the user
+     * @method onItemRemoved
+     */
+    onItemRemoved: function (params) {
+        this.select.showChoice({
+            value: params
+        });
+        this.select.el.selectedIndex = 0;
+        this.fireUpdatedEvt();
+    },
+
+    /**
+     * Add an item to the list when the select changed
+     * @method onAddNewItem
+     */
+     onAddNewItem: function () {
+
+        var value, position, choice;
+
+        if (this.select.el.selectedIndex !== 0) {
+
+            // Get the selector value
+            value = this.select.getValue();
+
+            position = this.select.getChoicePosition({
+                value: value
+            });
+            choice = this.select.choicesList[position];
+
+            inputEx.MultiSelectField.superclass.addItem.call(this, {
+                value: value,
+                label: choice.label
             });
 
-        },
-
-        /**
-         * Register the "change" event
-         * @method initEvents
-         */
-        initEvents: function () {
-            Y.on("change", this.onAddNewItem, this.el, this);
-            this.ddlist.on("itemRemoved", this.onItemRemoved, this);
-            this.ddlist.on("updated", this.fireUpdatedEvt, this);
-        },
-
-        /**
-         * Re-enable the option element when an item is removed by the user
-         * @method onItemRemoved
-         */
-        onItemRemoved: function (params) {
-
-            this.showChoice({
-                value: params
+            // hide choice that has just been selected (+ select first choice)
+            this.select.hideChoice({
+                position: position
             });
-            this.el.selectedIndex = 0;
+            this.select.el.selectedIndex = 0;
 
             this.fireUpdatedEvt();
 
-        },
+        }
+    },
 
-        /**
-         * Add an item to the list when the select changed
-         * @method onAddNewItem
-         */
-        onAddNewItem: function () {
+    /**
+     * Set the value of the list
+     * @method setValue
+     * @param {String} value The value to set
+     * @param {boolean} [sendUpdatedEvt] (optional) Wether this setValue should fire the 'updated' event or not (default is true, pass false to NOT send the event)
+     */
+    setValue: function (value, sendUpdatedEvt) {
 
-            var value, position, choice;
+        var i, length, position, choice, ddlistValue = [];
 
-            if (this.el.selectedIndex !== 0) {
-               
-               // Get the selector value
-               value = inputEx.MultiSelectField.superclass.getValue.call(this);
-               
-               position = this.getChoicePosition({
-                  value: value
-               });
-               choice = this.choicesList[position];
-               
-               this.ddlist.addItem({
-                  value: value,
-                  label: choice.label
-               });
-               
-               // hide choice that has just been selected (+ select first choice)
-               this.hideChoice({
-                  position: position
-               });
-               this.el.selectedIndex = 0;
-               
-               this.fireUpdatedEvt();
-               
-            }
-        },
-
-        /**
-         * Set the value of the list
-         * @method setValue
-         * @param {String} value The value to set
-         * @param {boolean} [sendUpdatedEvt] (optional) Wether this setValue should fire the 'updated' event or not (default is true, pass false to NOT send the event)
-         */
-        setValue: function (value, sendUpdatedEvt) {
-
-            var i, length, position, choice, ddlistValue = [];
-
-            if (!lang.isArray(value)) {
-                return;
-            }
-
-            // Re-show all choices
-            for (i = 0, length = this.choicesList.length; i < length; i += 1) {
-                this.showChoice({
-                    position: i
-                });
-            }
-
-            // Hide selected choices and fill ddlist value
-            for (i = 0, length = value.length; i < length; i += 1) {
-
-                position = this.getChoicePosition({
-                    value: value[i]
-                });
-                choice = this.choicesList[position];
-
-                ddlistValue.push({
-                    value: choice.value,
-                    label: choice.label
-                });
-
-                this.hideChoice({
-                    position: position
-                });
-            }
-
-            // set ddlist value
-            this.ddlist.setValue(ddlistValue);
-
-            // reset select to first choice
-            this.el.selectedIndex = 0;
-
-            if (sendUpdatedEvt !== false) {
-                // fire update event
-                this.fireUpdatedEvt();
-            }
-        },
-
-        clear: function (sendUpdatedEvt) {
-           this.ddlist.clear();
-           this.setValue(lang.isUndefined(this.options.value) ? [] : this.options.value, sendUpdatedEvt);
-        },
-
-       enable: function() {
-          this.ddlist.enable();
-          inputEx.MultiSelectField.superclass.enable.call(this);
-       },
-
-       disable: function() {
-          this.ddlist.disable();
-          inputEx.MultiSelectField.superclass.disable.call(this);
-       },
-
-       /**
-         * Return the value
-         * @method getValue
-         * @return {Any} an array of selected values
-         */
-        getValue: function () {
-            return this.ddlist.getValue();
-        },
-
-        isEmpty: function () {
-            return this.getValue().length === 0;
+        if (!lang.isArray(value)) {
+            return;
         }
 
-    });
+        // Re-show all choices
+        for (i = 0, length = this.select.choicesList.length; i < length; i += 1) {
+            this.select.showChoice({
+                position: i
+            });
+        }
 
-    // Register this class as "multiselect" type
-    inputEx.registerType("multiselect", inputEx.MultiSelectField);
+        // Hide selected choices and fill ddlist value
+        for (i = 0, length = value.length; i < length; i += 1) {
+            position = this.select.getChoicePosition({
+                value: value[i]
+            });
+            choice = this.select.choicesList[position];
+
+            ddlistValue.push({
+                value: choice.value,
+                label: choice.label
+            });
+
+            this.select.hideChoice({
+                position: position
+            });
+        }
+
+        // set ddlist value
+        inputEx.MultiSelectField.superclass.setValue.call(this, ddlistValue);
+
+        // reset select to first choice
+        this.select.el.selectedIndex = 0;
+
+        if (sendUpdatedEvt !== false) {
+            // fire update event
+            this.fireUpdatedEvt();
+        }
+    },
+
+    clear: function (sendUpdatedEvt) {
+        inputEx.MultiSelectField.superclass.clear.call(this);
+        this.select.setValue(lang.isUndefined(this.options.value) ? [] : this.options.value, sendUpdatedEvt);
+    },
+
+    enable: function() {
+        inputEx.MultiSelectField.superclass.enable.call(this);
+        this.select.enable();
+    },
+
+    disable: function() {
+        inputEx.MultiSelectField.superclass.disable.call(this);
+        this.select.disable();
+    },
+
+    isEmpty: function () {
+        return this.getValue().length === 0;
+    }
+
+});
+
+// Register this class as "multiselect" type
+inputEx.registerType("multiselect", inputEx.MultiSelectField);
